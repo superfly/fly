2dc6ae70b0acd766f50aca6e03995c0119abadb5
Jerome Gravel-Niquet <jeromegn@gmail.com>
2018-01-17 17:23:03 -0500

    feat(cli) adds --app option or app.id (in your .fly.yml) needs to be set.

---
+++ src/cmd/deploy.ts
@@ -0,0 +1,29 @@
+import axios from 'axios'
+const { buildApp } = require('../utils/build')
+
+const flyEndpoint = process.env.FLY_BASE_URL || "https://fly.io"
+
+module.exports = function deployApp(appID: string, token: string) {
+  buildApp(process.cwd(), { watch: false }, (err: Error, code: string) => {
+    if (err) {
+      throw err
+    }
+
+    axios({
+      method: 'put',
+      url: `${flyEndpoint}/api/v1/sites/${appID}/script`,
+      headers: {
+        'Authorization': `Bearer ${token}`
+      },
+      data: {
+        script: code
+      }
+    }).then((res) => {
+      if (res.status === 200 && res.data.meta.updated) {
+        console.log("Successfully updated app on Fly.")
+        process.exit(0)
+      }
+    })
+
+  })
+}
