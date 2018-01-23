a2e27fc522bd73d9100d3a282ddf8321683d93b4
Kurt <mrkurt@gmail.com>
2018-01-22 21:02:15 -0600

    catch errors when retrieving script from fly

--- src/cmd/fetch.ts
+++ src/cmd/fetch.ts
@@ -3,17 +3,22 @@ const { buildApp } = require('../utils/build')
 
 const flyEndpoint = process.env.FLY_BASE_URL || "https://fly.io"
 
-module.exports = function deployApp(appID: string, token: string) {
-  axios({
-    method: 'get',
-    url: `${flyEndpoint}/api/v1/sites/${appID}/script`,
-    headers: {
-      'Authorization': `Bearer ${token}`
-    }
-  }).then((res) => {
+module.exports = async function deployApp(appID: string, token: string) {
+  try{
+    let res = await axios({
+      method: 'get',
+      url: `${flyEndpoint}/api/v1/sites/${appID}/script`,
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+
     if (res.status === 200) {
       console.log(res.data.script)
       process.exit(0)
     }
-  })
+  }catch(err){
+    console.error(err.message)
+    process.exit(1)
+  }
 }
