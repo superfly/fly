d5c873be0d1b67395b71adc0077bab8bccd26fa2
Jerome Gravel-Niquet <jeromegn@gmail.com>
2018-01-25 14:53:58 -0500

    fix: export more types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 export { Server } from './server'
-export { Context } from './context'
+export { Context, createContext } from './context'
+export { ContextStore } from './context_store'
 export { Config } from './config'
 export { AppStore } from './app/store'
 export { App } from './app'
 export { CacheStore } from './cache_store'
 export { registerBridge } from './bridge'
+export { v8Env } from './v8env'
 import * as ivm from 'isolated-vm'
 // export const ivm = require('isolated-vm')
 export { ivm }
