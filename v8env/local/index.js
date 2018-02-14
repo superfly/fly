c99af05a2ffbd14a48befeead6e52493d716bb32
Jerome Gravel-Niquet <jeromegn@gmail.com>
2018-02-14 12:56:25 -0500

    try with a longer timeout

--- v8env/local/index.js
+++ v8env/local/index.js
@@ -1,8 +1,6 @@
 global.localBootstrap = function localBootstrap() {
-  global.flyLog = function (lvl, ...args) {
-    _log.apply(null, [lvl].concat(args))
-  }
   addEventListener('log', (event) => {
-    flyLog(event.log.level, event.log.message)
+    if (global._log)
+      _log.apply(null, [event.log.level, event.log.message])
   })
 }
