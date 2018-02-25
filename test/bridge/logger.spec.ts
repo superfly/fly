57088f56b2ce4160f41e3e14f673046aa769d3f8
Jerome Gravel-Niquet <jeromegn@gmail.com>
2018-02-25 10:19:30 -0500

    feat: fly.log.addMetadata to add default metadata for all log entries

--- test/bridge/logger.spec.ts
+++ test/bridge/logger.spec.ts
@@ -13,6 +13,7 @@ describe('logger', () => {
         socket.bind(3334, "127.0.0.1", () => resolve(socket))
       })
     })
+
     after(async function () {
       await Promise.all([
         new Promise((resolve) => { this.server.close(resolve) }),
@@ -21,15 +22,25 @@ describe('logger', () => {
     })
 
     it('send a datagram to the defined syslog server', async function () {
-      const [msg, res] = await Promise.all([
-        new Promise((resolve) => {
-          this.udpServer.once('message', (message: Buffer) => resolve(message))
-        }),
-        axios.get("http://127.0.0.1:3333/", { headers: { 'Host': "test" } })
-      ])
+      let msgs: Buffer[] = []
+      this.udpServer.on('message', (message: Buffer) => {
+        msgs.push(message)
+      })
+
+      const res = await axios.get("http://127.0.0.1:3333/", { headers: { 'Host': "test" } })
 
       expect(res.status).to.equal(200)
-      expect(msg.toString()).to.include("info: yo")
+
+      const msg1Str: string = msgs[0].toString()
+      expect(msg1Str).to.include("info: yo")
+      expect(msg1Str).to.include("method=GET")
+      expect(msg1Str).to.include("hello=world")
+
+      const msg2Str: string = msgs[1].toString()
+      expect(msg2Str).to.include("debug: debug test")
+      expect(msg2Str).to.include("method=GET")
+      expect(msg2Str).to.include("foo=bar")
+      expect(msg2Str).to.include("hello=notworld")
     })
 
   })
