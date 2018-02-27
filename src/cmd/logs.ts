7e127667b667bbf1e97036cd41d3f08b9ed15b8d
Mat Byczkowski <mbyczkowski@gmail.com>
2018-02-27 05:47:01 -0800

    fix(cli): display an error message if app doesn't have logs, yet (#28)

--- src/cmd/logs.ts
+++ src/cmd/logs.ts
@@ -1,5 +1,6 @@
 import { root, getAppId } from './root'
 import { API } from './api'
+import { processResponse } from '../utils/cli'
 
 import colors = require('ansi-colors')
 
@@ -22,7 +23,7 @@ async function continuouslyGetLogs(appID: string) {
       showLogs(logs)
     } catch (e) {
       if (e.response)
-        console.log(e.response.data)
+        processResponse(e.response)
       else
         console.log(e)
       break;
@@ -55,13 +56,27 @@ async function showLogs(logs: Log[]) {
   }
 }
 
-async function getLogs(appID: string, nextToken?: string): Promise<[any[], string]> {
+async function getLogs(appID: string, nextToken?: string): Promise<[any[], string | undefined]> {
   const res = await API.get(`/api/v1/apps/${appID}/logs`, {
     params: { next_token: nextToken }
   })
-  return [res.data.data, res.data.meta.next_token]
+  if (res.data.data && res.data.data.meta) {
+    return [res.data.data, res.data.meta.next_token]
+  }
+  // no data that we want, likely an error, continuouslyGetLogs will catch and
+  // process errors accordingly
+  throw new LogResponseError(res)
 }
 
 function sleep(i: number) {
   return new Promise((res) => setTimeout(res, i))
+}
+
+class LogResponseError extends Error {
+  response: Object
+
+  constructor(response: Object, ...params: any[]) {
+    super(...params)
+    this.response = response
+  }
 }
