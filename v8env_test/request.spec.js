c839eb19ac62e59ecabd481ce5be94a8a1b18ba6
zoe <z.zoelec2@gmail.com>
2018-04-06 08:26:45 -0700

    fix: Remove unneeded code

--- v8env_test/request.spec.js
+++ v8env_test/request.spec.js
@@ -1,11 +1,8 @@
 import { expect } from 'chai'
 
 describe("Request", () => {
-  const text = "helloFLY"
+  const text = "helloFLY" //This must be a multiple of 8 charictars long (eg. 8, 16, 24)
   const buffer = new TextEncoder("utf-8").encode(text).buffer
-  const buffer16 = new TextEncoder("utf-16").encode(text).buffer
-  const buffer32 = new TextEncoder("utf-32").encode(text).buffer
-  const buffer64 = new TextEncoder("utf-64").encode(text).buffer
 
   it("can be instantiated", () => {
     expect(new Request("http://example.com")).not.to.throw
@@ -43,7 +40,7 @@ describe("Request", () => {
   })
 
   it('returns an ArrayBuffer given a Uint16Array', async () => {
-    const r = new Response(new Uint16Array(buffer16))
+    const r = new Response(new Uint16Array(buffer))
 
     expect(new TextDecoder("utf-8").decode(await r.arrayBuffer())).to.equal(text)
   })
@@ -72,16 +69,3 @@ describe("Request", () => {
     expect(new TextDecoder("utf-8").decode(await r.arrayBuffer())).to.equal(text)
   })
 })
-
-function ab2str(buf) {
-  return String.fromCharCode.apply(null, buf);
-}
-
-function str2ab(str) {
-  var buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
-  var bufView = new Uint16Array(buf);
-  for (var i = 0, strLen = str.length; i < strLen; i++) {
-    bufView[i] = str.charCodeAt(i);
-  }
-  return buf;
-}
