332356cc4a49217101a24962b801e61dd1961b4a
Zoe Carver <pudility@users.noreply.github.com>
2018-04-06 14:07:19 -0700

    fix: Support all TypedArray Response/Request bodies (#54)
    The Body Mixin now properly handles all the various TypedArrays.

--- v8env/ts/body_mixin.ts
+++ v8env/ts/body_mixin.ts
@@ -93,9 +93,17 @@ export default class BodyMixin implements Body {
   }
 
   async arrayBuffer(): Promise<ArrayBuffer> {
-    if (this.bodySource instanceof Uint8Array) {
-      const buffer = this.bodySource.buffer
-      if (buffer instanceof ArrayBuffer) return buffer
+    if (this.bodySource instanceof Int8Array ||
+        this.bodySource instanceof Int16Array ||
+        this.bodySource instanceof Int32Array ||
+        this.bodySource instanceof Uint8Array ||
+        this.bodySource instanceof Uint16Array ||
+        this.bodySource instanceof Uint32Array ||
+        this.bodySource instanceof Uint8ClampedArray ||
+        this.bodySource instanceof Float32Array ||
+        this.bodySource instanceof Float64Array
+    ) {
+      return <ArrayBuffer>this.bodySource.buffer
     } else if (this.bodySource instanceof ArrayBuffer) {
       return this.bodySource
     } else if (typeof this.bodySource === 'string') {
@@ -146,6 +154,7 @@ function bufferFromStream(stream: ReadableStreamReader): Promise<ArrayBuffer> {
     })()
   })
 }
+
 /** @hidden */
 function concatenate(...arrays: Uint8Array[]): ArrayBuffer {
   let totalLength = 0;
