6135151807a6e385f0660f9ccba6bd80c895ec18
zoe <z.zoelec2@gmail.com>
2018-04-06 13:16:26 -0700

    fix: Support decoding gzip content

--- test/fixtures/apps/gzip-fetch.js
+++ test/fixtures/apps/gzip-fetch.js
@@ -1,5 +1,5 @@
 fly.http.respondWith(async function(req){
-  let resp = await fetch("https://example.com")
+  let resp = await fetch("https://example.com", { headers: {"Accept-Encoding": "gzip"}, inflate: true })
   let txt = await resp.text()
   console.log("Got text: ", txt.length)
   return new Response(txt, resp)
