1259a7661876d8c11bbfb4cf87411289a1450624
zoe <z.zoelec2@gmail.com>
2018-04-10 10:25:10 -0700

    Fix server issue

--- apps/fly-fleet/index.js
+++ apps/fly-fleet/index.js
@@ -1,5 +1,7 @@
 fly.http.respondWith(async (req) => {
-  const res = await fetch("file://index.html")
+  const url = new URL(req.url)
+  const res = await fetch("file:/" + url.pathname)
   res.headers.set("content-type", "text/html")
+  cache.put(req, res)
   return res
 })
