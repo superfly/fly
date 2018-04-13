ddd792e4a68c2e4a7486d2a48e9c26e6aa3350a7
zoe <z.zoelec2@gmail.com>
2018-04-13 15:16:49 -0700

    fix: remove unneeded packages

---
+++ src/fontmin.d.ts
@@ -0,0 +1,11 @@
+export = Fontmin;
+
+declare class Fontmin {
+    constructor();
+
+    src(file: any):any
+    use(plugin: any):any
+    dest(folder:string):any
+    static glyph(obj:any):any
+    static ttf2woff(obj:any):any
+}
