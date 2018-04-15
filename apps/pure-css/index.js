785555ae80e329beb79729884e77e2bac7c96536
zoe <z.zoelec2@gmail.com>
2018-04-15 08:43:31 -0700

    fix: update example

--- apps/pure-css/index.js
+++ apps/pure-css/index.js
@@ -1,4 +1,14 @@
 fly.http.respondWith(async (req) => {
+	const path = new URL(req.url).pathname
+	if (path !== '/') {
+		const file = await fetch('file:/' + path)
+		const contents = await file.text()
+		return new Response(contents, {
+    	headers: {
+      	"Content-Type": "text/html"
+    	}
+  	})
+	}	
 	const css = await fetch('file://bootstrap.min.css');
 	const html = await fetch ('file://hello.html');
 	const minCSS = fly.CSS(await css.text(), await html.text());
