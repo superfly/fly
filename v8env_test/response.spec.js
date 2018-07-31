47cb7135c703d3221a0a9d3c5ccd8ab00a3c4620
Kurt <mrkurt@gmail.com>
2018-07-31 18:25:27 -0400

    Add tests for Response.redirect

--- v8env_test/response.spec.js
+++ v8env_test/response.spec.js
@@ -8,3 +8,15 @@ describe("Response", () => {
     expect(() => new Response(["wat"])).to.throw(/Bad Response body type/)
   })
 })
+describe("Response.redirect", () => {
+  it("creates a 302 by default", () => {
+    const r = Response.redirect("http://test.com")
+    expect(r.status).to.eq(302)
+    expect(r.headers.get("location")).to.eq("http://test.com")
+  })
+
+  it('allows a custom status', () => {
+    const r = Response.redirect("http://test.com", 307)
+    expect(r.status).to.eq(307)
+  })
+})
