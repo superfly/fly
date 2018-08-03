6088889a074c5dcdcf67f29f5e2093b4654684ca
Kurt <mrkurt@gmail.com>
2018-08-03 18:26:25 -0500

    Added docs for @fly/data, because why not?

--- v8env/src/fly/data.ts
+++ v8env/src/fly/data.ts
@@ -1,16 +1,30 @@
 /**
+ * Persistent, global key/value data store. Open collections, write data with `put`. Then retrieve data with `get`.
+ * 
+ * Keys and values are stored in range chunks. Chunks migrate to the region they're most frequently accessed from.
  * @module fly/data
- * @private
- * Needs docs + release
  */
 declare var bridge: any
 
+/**
+ * A collection of keys and values.
+ */
 export class Collection {
   name: string
+
+  /**
+   * Opens a collection
+   * @param name name of the collection to open
+   */
   constructor(name: string) {
     this.name = name
   }
 
+  /**
+   * Stores data in the collection associated key
+   * @param key key for data
+   * @param obj value to store
+   */
   put(key: string, obj: string) {
     return new Promise((resolve, reject) => {
       try {
@@ -27,6 +41,10 @@ export class Collection {
     })
   }
 
+  /**
+   * Retrieves data from the collection store
+   * @param key key to retrieve
+   */
   get(key: string) {
     return new Promise((resolve, reject) => {
       bridge.dispatch("fly.Data.get", this.name, key, (err: string | null, res: any) => {
@@ -43,6 +61,10 @@ export class Collection {
     })
   }
 
+  /**
+   * Deletes data from the collection store.
+   * @param key key to delete
+   */
   del(key: string) {
     return new Promise((resolve, reject) => {
       bridge.dispatch("fly.Data.del", this.name, key, (err: string | null, ok: boolean) => {
