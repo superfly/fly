1b553e324cfbca0deecf8c01d63ab2310fb72c0a
Michael Dwan <m@dwan.io>
2018-09-05 20:24:54 -0500

    Move over failure tests

---
+++ tests/e2e/failures-js/invalid.tests.ts
@@ -0,0 +1,22 @@
+import { AppConfig } from "@fly/test-server"
+import * as path from "path"
+import * as fs from "fs"
+
+declare function setupApps(appConfig: AppConfig): void
+
+describe("Invalid Apps", () => {
+  const invalidApps = fs.readdirSync(path.resolve(__dirname, "invalid"))
+
+  for (const appPath of invalidApps) {
+    describe(appPath, () => {
+      console.log("setup", appPath)
+      setupApps({ "edge.test": path.resolve(__dirname, "invalid", appPath) })
+
+      test("returns 500", async () => {
+        const response = await fetch("http://edge.test")
+        expect(response.status).toEqual(500)
+      })
+    })
+  }
+})
+
