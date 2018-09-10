b84c513d7d40756cc2bb7ec54eef569840ef594b
Jon Phenow <j.phenow@gmail.com>
2018-09-10 13:17:05 -0500

    feat: configurable `responseCache` headers
    The `responseCache` now supports more flexible header caching. Specify which headers you care about when you cache a response:
    
    ```javascript
    import { responseCache } from "@fly/cache"
    
    responseCache.set("key", resp, { skipCacheHeaders: ["authorization", "set-cookie"] }) // default
    
    responseCachce.set("key", resp, { skipCacheHeaders: ["content-length"] }) // custom + default
    ```

--- v8env/test/fly.cache.response.spec.js
+++ v8env/test/fly.cache.response.spec.js
@@ -3,10 +3,11 @@ import { expect } from 'chai'
 import cache, { responseCache } from "@fly/cache"
 
 let counter = 0
-async function makeResponse() {
+async function makeResponse(initResponseOptions, setCacheOptions) {
   let key = `cache-test-key-${counter++}`
-  const resp = new Response("hi", { status: 404 })
-  const setResult = await responseCache.set(key, resp)
+  const responseInitOptions = Object.assign({ status: 404 }, initResponseOptions)
+  const resp = new Response("hi", responseInitOptions)
+  const setResult = await responseCache.set(key, resp, setCacheOptions)
 
   expect(setResult).to.eq(true)
   return [key, resp]
@@ -22,6 +23,47 @@ describe("@fly/cache/response", () => {
 
     expect(cachedBody).to.eq(body)
     expect(cachedResponse.status).to.eq(404)
+  })
+
+  it("sets a Response with headers", async () => {
+    const [key, resp] = await makeResponse({ headers: { "authorization": "foo", "content-type": "text/plain; charset=utf-8" } })
+    const cachedResponse = await responseCache.get(key)
+
+    expect(cachedResponse).instanceOf(Response)
+    const cachedBody = await cachedResponse.text()
+    const body = await resp.text()
+
+    expect(cachedBody).to.eq(body)
+    expect(cachedResponse.status).to.eq(404)
+
+    const meta = await responseCache.getMeta(key)
+    expect(meta.headers["authorization"]).to.eq(undefined)
+    expect(meta.headers["content-type"]).to.eq("text/plain; charset=utf-8")
+  })
+
+  it("sets a Response with headers, and custom skip-headers", async () => {
+    const [key, resp] = await makeResponse({
+      headers: {
+        "authorization": "foo",
+        "content-type": "text/plain; charset=utf-8",
+        "content-length": "538",
+        "content-encoding": "gzip"
+      }
+    }, { skipCacheHeaders: ["content-length"] })
+    const cachedResponse = await responseCache.get(key)
+
+    expect(cachedResponse).instanceOf(Response)
+    const cachedBody = await cachedResponse.text()
+    const body = await resp.text()
+
+    expect(cachedBody).to.eq(body)
+    expect(cachedResponse.status).to.eq(404)
+
+    const meta = await responseCache.getMeta(key)
+    expect(meta.headers["authorization"]).to.eq(undefined)
+    expect(meta.headers["content-length"]).to.eq(undefined)
+    expect(meta.headers["content-type"]).to.eq("text/plain; charset=utf-8")
+    expect(meta.headers["content-encoding"]).to.eq("gzip")
   })
 
   it("deletes a response", async () => {
