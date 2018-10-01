c146f28da4e757adc07101a19183e718f6c4dcf8
Jon Phenow <jophenow@microsoft.com>
2018-10-01 14:15:54 -0500

    reformat cookie-jar-spec

--- tests/v8env/tests/cookie_jar.spec.js
+++ tests/v8env/tests/cookie_jar.spec.js
@@ -1,8 +1,8 @@
-import { expect } from 'chai'
+import { expect } from "chai"
 
 describe("cookies", () => {
   it("gets cookie values", () => {
-    const req = new Request("http://example.com", { headers: { cookie: 'foo=bar;hello=world;' } })
+    const req = new Request("http://example.com", { headers: { cookie: "foo=bar;hello=world;" } })
     const foo = req.cookies.get("foo").value
     const hello = req.cookies.get("hello").value
     const no = req.cookies.get("no").value
