acde682cb346117e8053a1d0f941b62979ff419c
Selwyn <talk@selwyn.cc>
2018-10-26 11:08:01 +0200

    Remove webpack dependencies from core

--- packages/core/src/v8env.ts
+++ packages/core/src/v8env.ts
@@ -2,8 +2,6 @@ import * as path from "path"
 import * as fs from "fs"
 import * as semver from "semver"
 
-import * as webpack from "webpack"
-
 import { EventEmitter } from "events"
 import { createHash } from "crypto"
 import * as ivm from "isolated-vm"
