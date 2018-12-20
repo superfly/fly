1842661f73508d62d3a3306a09b1975507e498a3
Michael Dwan <m@dwan.io>
2018-12-20 16:38:50 -0600

    Resolve relative paths when creating URLs

---
+++ tests/v8env/tests/url.spec.js
@@ -0,0 +1,24 @@
+import { expect } from "chai"
+
+describe("URL", () => {
+  it("ensures pathname begins with a slash", () => {
+    const url = new URL("https://fly.io/about")
+    url.pathname = "team"
+    expect(url.pathname).to.equal("/team")
+  })
+
+  it("resolves a relative path from a base", () => {
+    const url = new URL("fly", new URL("https://github.com/superfly/"))
+    expect(url.pathname).to.equal("/superfly/fly")
+  })
+
+  it("resolves an absolte path from a base", () => {
+    const url = new URL("/about", new URL("https://fly.io/path/"))
+    expect(url.pathname).to.equal("/about")
+  })
+
+  it("inserts trailing slash if needed", () => {
+    const url = new URL("fly", new URL("https://github.com/superfly"))
+    expect(url.pathname).to.equal("/superfly/fly")
+  })
+})
