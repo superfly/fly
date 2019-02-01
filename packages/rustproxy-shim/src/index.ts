6e73b839f147abdd3261ab8538a8a7abb1bbb80e
Jerome Gravel-Niquet <jeromegn@gmail.com>
2019-02-01 12:05:32 -0500

    assign MiddlewareChain to gloabl, in shim

--- packages/rustproxy-shim/src/index.ts
+++ packages/rustproxy-shim/src/index.ts
@@ -1,11 +1,13 @@
 import * as bridge from "./bridge"
 import * as domUtils from "./document"
+import { MiddlewareChain } from '../../v8env/src/middleware'
 
 if (window.libfly) {
   console.log("rustproxy detected, installing rustproxy-shim")
 
   Object.assign(window, {
     bridge,
+    MiddlewareChain,
     ...domUtils,
     app: window.fly.app
   })
