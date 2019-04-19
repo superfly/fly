9705d0e841ec2669402a67f04c47afdaef221ef6
Michael Dwan <m@dwan.io>
2019-04-19 15:46:15 -0500

    git cli commands

---
+++ packages/cli/src/commands/git/index.ts
@@ -0,0 +1,21 @@
+import { FlyCommand } from "../../base-command"
+import * as sharedFlags from "../../flags"
+import { getAppName } from "../../util"
+
+export default class Git extends FlyCommand {
+  public static description = "git deployment tools for an app"
+
+  public static flags = { env: sharedFlags.env(), app: sharedFlags.app() }
+
+  static hidden = false
+
+  public async run() {
+    const { flags } = this.parse(Git)
+
+    const appName = getAppName(flags)
+
+    if (appName) {
+      console.log(`Git URL: https://git.fly.io/${appName}.git`)
+    }
+  }
+}
