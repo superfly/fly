59356e28bf6465ae1a80f5208d91f1882bb9bb31
Michael Dwan <m@dwan.io>
2019-05-07 08:28:16 -0500

    remove old packages

--- packages/build/src/main.ts
+++ packages/build/src/main.ts
@@ -1,7 +1,5 @@
 import * as fs from "fs"
-// import * as glob from "glob"
 import * as path from "path"
-// import * as tar from "tar-fs"
 import * as webpack from "webpack"
 
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
