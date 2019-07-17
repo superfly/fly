7c266b3d55598464404bfd98c5987ae3fdf309d4
Jerome Gravel-Niquet <jeromegn@gmail.com>
2019-07-17 11:07:39 -0400

    allow filtering by region and instance id

--- packages/cli/src/commands/logs.ts
+++ packages/cli/src/commands/logs.ts
@@ -2,6 +2,7 @@ import { FlyCommand } from "../base-command"
 import * as sharedFlags from "../flags"
 import { AxiosInstance } from "axios"
 import chalk from "chalk"
+import { flags as cmdFlags } from "@oclif/command"
 
 export default class Logs extends FlyCommand {
   public static description = "logs for an app"
@@ -9,7 +10,17 @@ export default class Logs extends FlyCommand {
   public static flags = {
     env: sharedFlags.env(),
     app: sharedFlags.app(),
-    token: sharedFlags.apiToken()
+    token: sharedFlags.apiToken(),
+    instance: cmdFlags.string({
+      description: "Instance ID to filter",
+      char: "i",
+      required: false,
+    }),
+    region: cmdFlags.string({
+      description: "Region to filter",
+      char: "r",
+      required: false,
+    })
   }
 
   public async run() {
@@ -18,15 +29,21 @@ export default class Logs extends FlyCommand {
     const API = this.apiClient(flags)
     const appName = this.getAppName(flags)
 
-    await this.continuouslyGetLogs(API, appName)
+    const opts: LogOptions = {}
+    if (flags.region)
+      opts.region = flags.region
+    else if (flags.instance)
+      opts.instance = flags.instance
+
+    await this.continuouslyGetLogs(API, appName, opts)
   }
 
-  async continuouslyGetLogs(API: AxiosInstance, appName: string) {
+  async continuouslyGetLogs(API: AxiosInstance, appName: string, opts?: LogOptions) {
     let lastNextToken: string | undefined
     while (true) {
       let count = 0
       try {
-        const [logs, nextToken] = await getLogs(API, appName, lastNextToken)
+        const [logs, nextToken] = await getLogs(API, appName, lastNextToken, opts)
         count = logs.length
         lastNextToken = nextToken
         showLogs(logs)
@@ -73,17 +90,26 @@ async function showLogs(logs: Log[]) {
     const ts = new Date(l.attributes.timestamp)
     const lvl = levels[parseInt(l.attributes.level, 10)] || l.attributes.level
     const levelColor = levelColorFn[lvl] || chalk.white
-    console.log(
-      `${chalk.dim(ts.toISOString().split(".")[0] + "Z")} ${chalk.green(region)} [${levelColor(lvl)}] ${
-        l.attributes.message
-      }`
-    )
+    const instance = l.attributes.meta.instance
+    let logLine = chalk.dim(ts.toISOString().split(".")[0] + "Z")
+    if (instance) {
+      logLine += ` ${instance}`
+    }
+    logLine += ` ${chalk.green(region)} [${levelColor(lvl)}] ${
+      l.attributes.message
+    }`
+    console.log(logLine)
   }
 }
 
-async function getLogs(API: AxiosInstance, appName: string, nextToken?: string): Promise<[any[], string | undefined]> {
+interface LogOptions {
+  region?: string,
+  instance?: string,
+}
+
+async function getLogs(API: AxiosInstance, appName: string, nextToken: string | undefined, opts?: LogOptions): Promise<[any[], string | undefined]> {
   const res = await API.get(`/api/v1/apps/${appName}/logs`, {
-    params: { next_token: nextToken }
+    params: Object.assign({}, opts, { next_token: nextToken })
   })
   if (res.data.data && res.data.meta) {
     return [res.data.data, res.data.meta.next_token || nextToken]
